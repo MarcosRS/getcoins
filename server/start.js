debd803fe74789328d5ef1bb2da9237bcb790c6c
Marcos Reynoso <marcos.reynoso@dowjones.com>
2019-03-21 17:50:04 -0400

    add: coins 1

--- server/start.js
+++ server/start.js
@@ -3,14 +3,24 @@ const coinJson =  require('./coin_list.json')
 const delay = require('../utils').delay;
 
 async function start() {
+        let count = 0
         for (let coinName in coinJson) {
             coinCal.getCoinInfo(coinName).then(result => {
                 Object.assign(coinJson[coinName], result)
             })
             await delay()
-            
+            console.log('count: ', count++);
         }
-        console.log('coinJson: ', JSON.stringify(coinJson));
+        
+        console.log('coinJson: ', coinJson);
+        console.log('Object.keys(coinJson).length: ', Object.keys(coinJson).length);
+        Object.values(coinJson).map(coin =>{
+            if (coin.currentPercent < 30){
+                console.log(coin);
+            }
+        })
+
+        // console.log('coinJson: ', JSON.stringify(coinJson));
 }
 
 
